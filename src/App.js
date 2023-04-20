7146d47f42bfdac7ce687f56544975533a0cba7a
devjjin <sujink34@naver.com>
2023-04-20 11:07:25 +0900

    Feat: todolist구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([{ id: 0, title: '리액트를 배워봅시다' }]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__container">
+      <CreateTodo todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} />
     </div>
   );
 }
+
+const CreateTodo = ({ todos, setTodos }) => {
+  const [title, setTitle] = useState('');
+  const idRef = useRef(0);
+
+  const handleTitleChange = e => setTitle(e.target.value);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const newTodo = {
+      id: idRef.current++,
+      title,
+    };
+    setTodos([...todos, newTodo]);
+    setTitle('');
+  };
+
+  return (
+    <div className="create__todo__container">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            <input value={title} onChange={handleTitleChange} />
+            <button type="submit">추가하기</button>
+          </div>
+          <h1>Todo List</h1>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const TodoList = ({ todos }) => {
+  return (
+    <div className="todo__list">
+      {todos.map(todo => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
+    </div>
+  );
+};
+
+const Todo = ({ todo }) => {
+  return <div className="todo__item">{todo.title}</div>;
+};
 
 export default App;
